1d7f218a14998d833dff9de7da44b267107903a1
Patrick Himmelmann <patrick.himmelmann@kit.edu>
2020-12-27 18:14:59 +0100

    refactor day 2 to still work

--- examples/2.rs
+++ examples/2.rs
@@ -1,26 +1,24 @@
 use aoc2019::get_input;
-use std::io::Read;
 
-fn main()->std::io::Result<()>{
+fn main() -> std::io::Result<()> {
 	println!("Day 2");
-	let mut input=get_input(2)?;
-	let mut buf=String::new();
-	input.read_to_string(&mut buf)?;
-	let mut mem=aoc2019::intcode::init_mem(&buf);
-	mem[1]=12;
-	mem[2]=2;
-	aoc2019::intcode::execute(&mut mem).expect("Error during execution");
-	println!("First solution: {}",mem[0]);
-	for noun in 0..=99{
-		for verb in 0..=99{
-			let mut mem=aoc2019::intcode::init_mem(&buf);
-			mem[1]=noun;
-			mem[2]=verb;
-			aoc2019::intcode::execute(&mut mem).expect("Error during execution");
-			if mem[0]==19690720{
-				println!("Second solution: {}",100*noun+verb);
-				return Ok(())
+	let input = get_input(2)?;
+	let base_machine = aoc2019::intcode::Machine::from_file(input);
+	let mut machine = base_machine.clone();
+	machine.set_mem(1, 12);
+	machine.set_mem(2, 2);
+	machine.run(None).expect("Error during execution");
 
+	println!("First solution: {}", machine.get_mem(0));
+	for noun in 0..=99 {
+		for verb in 0..=99 {
+			let mut machine = base_machine.clone();
+			machine.set_mem(1, noun);
+			machine.set_mem(2, verb);
+			machine.run(None).expect("Error during execution");
+			if machine.get_mem(0) == 19690720 {
+				println!("Second solution: {}", 100 * noun + verb);
+				return Ok(());
 			}
 		}
 	}
