488a33dfbf1b177c35a3cecd23bb3ecec60bda1d
Patrick Himmelmann <patrick.himmelmann@kit.edu>
2020-12-29 00:38:29 +0100

    (day 13) part 2

--- examples/13.rs
+++ examples/13.rs
@@ -8,5 +8,29 @@ fn main() -> std::io::Result<()> {
 	let (_finished, res) = m.run(None).expect("Error");
 	let first = res.chunks_exact(3).filter(|x| x[2] == 2).count();
 	println!("First solution {}", first);
+
+	let mut m = base_machine.clone();
+	m.set_mem(0, 2);
+	let mut input = 0;
+	let mut ballx = 0;
+	let mut paddle = 0;
+	let mut score = 0;
+	loop {
+		let (finished, res) = m.run(Some(input)).expect("Error");
+		for tile in res.chunks_exact(3) {
+			if tile[0] == -1 {
+				score = tile[2];
+			} else if tile[2] == 4 {
+				ballx = tile[0];
+			} else if tile[2] == 3 {
+				paddle = tile[0];
+			};
+		}
+		input = (ballx - paddle).signum();
+		if finished {
+			break;
+		}
+	}
+	println!("Second solution {}", score);
 	Ok(())
 }
