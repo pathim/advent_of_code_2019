96e440984ff1bad39944932021d98cb118e1d287
Patrick Himmelmann <patrick.himmelmann@kit.edu>
2021-05-31 17:20:53 +0200

    Add solution vor Day 1

---
+++ examples/1.rs
@@ -0,0 +1,25 @@
+use aoc2019::get_input;
+use std::io::{BufRead,BufReader};
+
+fn fuel_need(input:u64) -> u64{
+	(input/3).saturating_sub(2)
+}
+
+fn fuel_sum(input:u64)->u64{
+	if input==0{
+		return 0;
+	}
+	input+fuel_sum(fuel_need(input))
+}
+
+fn total_fuel_need(input:u64)->u64{
+	fuel_sum(fuel_need(input))
+}
+
+fn main() -> std::io::Result<()>{
+	let input=BufReader::new(get_input(1)?);
+	let mass=input.lines().map(|l|l.unwrap().parse::<u64>().unwrap()).collect::<Vec<u64>>();
+	println!("First result: {}",mass.iter().map(|&m|fuel_need(m)).sum::<u64>());
+	println!("Second result: {}",mass.iter().map(|&m|total_fuel_need(m)).sum::<u64>());
+	Ok(())
+}
